c4fd48e3cdc80938b2fd65a48982b9152cb9bd7f
Siim Haas <siim.haas@ellore.ee>
2017-05-07 00:08:28 +0300

    Encapsulate users creation

--- DAL/UsersRepository.cs
+++ DAL/UsersRepository.cs
@@ -1,24 +1,27 @@
 ﻿using Domain;
 using System.Collections.Generic;
+using System;
 
 namespace DAL
 {
     public class UsersRepository
     {
         private List<CarModel> cars;
+        private IEnumerable<UserModel> users;
 
         public UsersRepository()
         {
             cars = PopulateCarsList();
+            users = PopulateUsersList();
         }
-
+        
         private List<CarModel> PopulateCarsList()
         {
             CarModel carModel = new CarModel();
             return carModel.FillCars();
         }
 
-        public IEnumerable<UserModel> Get()
+        private IEnumerable<UserModel> PopulateUsersList()
         {
             return new List<UserModel>
             {
@@ -26,6 +29,11 @@ namespace DAL
                 new UserModel {id=1,Name="Teet",Cars=cars[2]},
                 new UserModel {id=2,Name="Simmo",Cars=cars[1]}
             };
+        }
+
+        public IEnumerable<UserModel> Get()
+        {
+            return users;
         }
     }
 }
